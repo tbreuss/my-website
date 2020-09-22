7dfd8be3cfe3bcfa102cd854cd14781696757757
tbreuss <thomasbreuss@gmx.ch>
2020-09-22 22:01:51 +0200

    Remove html list

--- frontend/views/ArticleListView.js
+++ frontend/views/ArticleListView.js
@@ -8,12 +8,12 @@ export const ArticleListView = {
   },
   view: () => m('.articles', [
     m('h2', 'Artikel'),
-    m('ul', [
-      ArticleModel.list.map((article) => m('li', m('p', [
-          m(m.route.Link, {href: '/artikel/' + article.slug}, article.title),
-          m('br'),
-          article.abstract
-        ]))
-      )])
+    m('div', [
+      ArticleModel.list.map((article) => m('p', [
+        m(m.route.Link, {href: '/artikel/' + article.slug}, article.title),
+        m('br'),
+        article.abstract
+      ]))
+    ])
   ])
 }
