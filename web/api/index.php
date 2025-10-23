b33d8478854231c22ccf32421977688f064b803c
Thomas Breuss <thomasbreuss@gmx.ch>
2025-10-23 06:15:31 +0200

    Update PHP to v8.0, and update deps

--- web/api/index.php
+++ web/api/index.php
@@ -4,7 +4,6 @@ use app\components\DB;
 use app\controllers\ArticleController;
 use app\controllers\PageController;
 use app\controllers\PhotoController;
-use flight\Engine;
 
 define('ROOT_DIR', dirname(__DIR__, 2));
 
@@ -19,30 +18,22 @@ require ROOT_DIR . '/vendor/autoload.php';
 $config = require ROOT_DIR . '/backend/config/main.php';
 DB::init($config['db']);
 
-$app = new Engine();
-
 #$app->set('flight.base_url', '/api');
-$app->set('flight.log_errors', true);
+Flight::set('flight.log_errors', true);
 
-$app->map('error', function(Throwable $ex) use ($app) {
-    $app->response()
-        ->clear()
-        ->status(500)
-        ->send();
+Flight::map('error', function() {
+    Flight::jsonHalt(['message' => 'An error occurred'], 500);
 });
 
-$app->map('notFound', function() use ($app) {
-    $app->response()
-        ->clear()
-        ->status(404)
-        ->send();
+Flight::map('notFound', function() {
+    Flight::jsonHalt(['message' => 'Route not found'], 404);
 });
 
-$app->route('GET /articles/latest', [ArticleController::class, 'actionLatest']);
-$app->route('GET /articles/@slug', [ArticleController::class, 'actionDetail']);
-$app->route('GET /articles', [ArticleController::class, 'actionList']);
-$app->route('GET /pages/@slug', [PageController::class, 'actionDetail']);
-$app->route('GET /photos/latest', [PhotoController::class, 'actionLatest']);
-$app->route('GET /photos', [PhotoController::class, 'actionList']);
+Flight::route('GET /articles/latest', [ArticleController::class, 'actionLatest']);
+Flight::route('GET /articles/@slug', [ArticleController::class, 'actionDetail']);
+Flight::route('GET /articles', [ArticleController::class, 'actionList']);
+Flight::route('GET /pages/@slug', [PageController::class, 'actionDetail']);
+Flight::route('GET /photos/latest', [PhotoController::class, 'actionLatest']);
+Flight::route('GET /photos', [PhotoController::class, 'actionList']);
 
-$app->start();
+Flight::start();
